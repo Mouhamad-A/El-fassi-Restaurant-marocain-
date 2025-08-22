84f0ed9e118437df9e639c08fe0addf7c7c40cfe
Mouhamad-A <mouhamad69140@gmail.com>
2025-08-22 17:05:15 +0200

    commit 7

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+const handleMobileMenu = () => {
+    const navMobileMenu = document.getElementById('navMobileMenu');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const mobileMenuClose = document.getElementById('mobileMenuClose');
+
+    const openMenu = () => {
+        mobileMenu.classList.add('mobile-menu--open')
+    };
+
+    const closeMenu = () => {
+        mobileMenu.classList.remove('mobile-menu--open')
+    }
+
+    navMobileMenu.addEventListener('click', openMenu)
+    mobileMenuClose.addEventListener('click', closeMenu)
+}
+
+handleMobileMenu();
+
+
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
